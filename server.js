c6a443b057765d556210cb61456ab05bba1c6118
Himanshu Gautam <himanshugautam9697@gmail.com>
2025-06-03 15:48:31 +0530

    third commit

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
-const express = require("express");
+import express from 'express';
+import colors from 'colors';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send({
-    message: "Welcome to eCommerce-App",
-  });
+  res.send(
+    "message: Welcome to eCommerce-App",
+  );
 });
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`);
+  console.log(`Server running on ${PORT}`.bgCyan.white);
 });
