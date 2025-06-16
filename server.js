e2a4f949ff1fb66071ef47390f8ea5fcd92fc40a
Himanshu Gautam <himanshugautam9697@gmail.com>
2025-06-16 14:03:41 +0530

    forth commit

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 import express from 'express';
 import colors from 'colors';
 import dotenv from 'dotenv';
+import morgan from 'morgan';
+import connectDB from './config/db.js';
+import authRoutes from './routes/authRoute.js'
 
 dotenv.config();
 
+connectDB();
+
 const app = express();
+
+app.use(express.json());
+app.use(morgan('dev'));
+app.use('/api/v1/auth', authRoutes);
 
 app.get("/", (req, res) => {
   res.send(
@@ -15,5 +24,5 @@ app.get("/", (req, res) => {
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`.bgCyan.white);
+  console.log(`Server running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan.white);
 });
