6553692e870ab7e88c6ab306b45280e01b305663
Himanshu-Gautam20 <himanshugautam9696@gmail.com>
2025-07-14 18:06:52 +0530

    updated

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -17,11 +17,13 @@ router.post("/register", registerController);
 //LOGIN || POST
 router.post("/login", loginController);
 
+//Forgot Password || POST
 router.post("/forgot-password", forgotPasswordController);
 
 //test routes
 router.get("/test", requireSignIn, isAdmin, testController);
 
+//protected route auth
 router.get("/user-auth", requireSignIn, (req, res) => {
   res.status(200).send({ ok: true });
 });
