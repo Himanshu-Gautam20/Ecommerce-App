0dd5cc50748e7df34ad8e45642da03306b668fb1
Himanshu-Gautam20 <himanshugautam9696@gmail.com>
2025-07-19 16:24:12 +0530

    context update

--- client/src/context/auth.js
+++ client/src/context/auth.js
@@ -1,35 +1,36 @@
-import { useState, useContext, createContext, useEffect} from "react";
+import { useState, useEffect, useContext, createContext } from "react";
 import axios from "axios";
 
-const AuthContext = createContext({children});
+const AuthContext = createContext();
+const AuthProvider = ({ children }) => {
+  const [auth, setAuth] = useState({
+    user: null,
+    token: "",
+  });
 
-const AuthProvider = () => {
-    const [auth, setAuth] = useState({
-        user: null,
-        token: ""
-    });
+  //default axios
+  axios.defaults.headers.common["Authorization"] = auth?.token;
 
-    axios.defaults.headers.common["Authorization"] = auth?.token;
+  useEffect(() => {
+    const data = localStorage.getItem("auth");
+    if (data) {
+      const parseData = JSON.parse(data);
+      setAuth({
+        ...auth,
+        user: parseData.user,
+        token: parseData.token,
+      });
+    }
+    //eslint-disable-next-line
+  }, []);
+  return (
+    <AuthContext.Provider value={[auth, setAuth]}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
 
-    useEffect(() => {
-        const data = localStorage.getItem("auth");
-        if (data) {
-            const parsedData = JSON.parse(data);
-            setAuth({
-                user: parsedData.user,
-                token: parsedData.token
-            });
-        }
-        // eslint-disable-next-line
-    }, [])
-
-    return (
-        <AuthContext.Provider value={[auth, setAuth]}>
-            {Children}
-        </AuthContext.Provider>
-    )
-}
-
+// custom hook
 const useAuth = () => useContext(AuthContext);
 
-export {AuthProvider, useAuth};
+export { useAuth, AuthProvider };
