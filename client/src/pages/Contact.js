e9f26c6e7eda187bbec000f74ca185cf8a76b79e
Himanshu-Gautam20 <himanshugautam9696@gmail.com>
2025-07-19 16:32:17 +0530

    page update

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -3,7 +3,7 @@ import Layout from "./../components/Layout/Layout";
 import { BiMailSend, BiPhoneCall, BiSupport } from "react-icons/bi";
 const Contact = () => {
   return (
-    <Layout title={"Ecommerce-App - Contact Us"}>
+    <Layout title={"Contact us"}>
       <div className="row contactus ">
         <div className="col-md-6 ">
           <img
@@ -15,8 +15,8 @@ const Contact = () => {
         <div className="col-md-4">
           <h1 className="bg-dark p-2 text-white text-center">CONTACT US</h1>
           <p className="text-justify mt-2">
-            if you have any query and any info about product feel free to call anytime we are 24X7
-            available to help you.
+            any query and info about prodduct feel free to call anytime we 24X7
+            vaialible
           </p>
           <p className="mt-3">
             <BiMailSend /> : www.help@ecommerceapp.com
--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,16 +1,15 @@
-import React from 'react'
-import Layout from './../components/Layout/Layout';
-import { AuthProvider } from '../context/auth';
-import { useAuth } from '../context/auth';
+import React from "react";
+import Layout from "./../components/Layout/Layout";
+import { useAuth } from "../context/auth";
 
-function HomePage() {
+const HomePage = () => {
   const [auth, setAuth] = useAuth();
-
   return (
-    <Layout title={'Ecommerce-App - Home'}>
-      <h1>Home Page</h1>
+    <Layout title={"Best offers "}>
+      <h1>HomePage</h1>
+      <pre>{JSON.stringify(auth, null, 4)}</pre>
     </Layout>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
